d5294e46785fe0412cb5c698c5abcb27271d1c33
xden78 <venera.opt@gmail.com>
2024-10-22 17:43:22 +0300

    1

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -2,7 +2,19 @@ package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
+        StringBuilder line = new StringBuilder();
+        for (int i = 1; i <= cathetusLength; i++) {
+            int space = cathetusLength - i;
+            for (int a = 1; a <= space; a++) {
+                line.append(" ");
+            }
 
+            for (int s = 1; s <= i; s++) {
+                line.append("*");
+            }
+            System.out.println(line);
+            line = new StringBuilder();
+        }
     }
 
     public static void main(String[] args) {
--- src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
@@ -1,6 +1,18 @@
 package school.mjc.stage0.loops.finalTask;
 
 public class MultiplicationTable {
-    public void printTable(int numberTableToPrint){
+    public static void printTable(int numberTableToPrint){
+       // "1 x 2 = 2\n"
+       // "2 x 2 = 4\n"
+        // StringBuilder output = new StringBuilder();
+         int result;
+       for (int i = 1; i <= 10; i++) {
+            result = i * numberTableToPrint;
+            System.out.println(i + " x " + numberTableToPrint + " = " + result);
+       }
+    }
+
+    public static void main(String[] args) {
+        printTable(2);
     }
 }
