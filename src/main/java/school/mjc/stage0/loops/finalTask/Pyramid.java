19b9b3ca381cdbe19f95dc18378d21e678487cba
xden78 <venera.opt@gmail.com>
2024-10-22 18:25:41 +0300

    2

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,6 +2,25 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
+        StringBuilder line = new StringBuilder();
+        for (int i = 1; i <= cathetusLength; i++) {
+            int space = cathetusLength - i;
+            for (int a = 1; a <= space; a++) {
+                line.append(" ");
+            }
+
+            for (int s = i; s > 0; s--) {
+                line.append(s);
+            }
+
+            for (int d = 2; d <= i; d++) {
+                line.append(d);
+            }
+            System.out.println(line);
+            line = new StringBuilder();
+        }
+
+
 
     }
 
